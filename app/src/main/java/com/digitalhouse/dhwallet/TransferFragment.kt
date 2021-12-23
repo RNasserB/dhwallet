94e69569584125682caa9fddfc9df711708675b8
RNasserB <rnbprojetos@gmail.com>
2021-12-23 19:37:35 -0300

    Implemento Recycler

--- app/src/main/java/com/digitalhouse/dhwallet/TransferFragment.kt
+++ app/src/main/java/com/digitalhouse/dhwallet/TransferFragment.kt
@@ -5,15 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.digitalhouse.dhwallet.adapter.ContactAdapter
+import com.digitalhouse.dhwallet.model.Contact
+import com.digitalhouse.dhwallet.model.ContactType
 
 class TransferFragment : Fragment(R.layout.fragment_transfer) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transfer, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val listContact = MutableList(10){
+            Contact(
+                image = "https://avatars.githubusercontent.com/u/1663531?v=4",
+                name = "Denis",
+                type = ContactType.IRMAO
+            )
+        }
+
+        val recycler = view.findViewById<RecyclerView>(R.id.recycler_contact)
+        recycler.adapter = ContactAdapter(listContact)
     }
 
 }
